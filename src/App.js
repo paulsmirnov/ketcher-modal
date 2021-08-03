5710506aad7abe0e9ebaf334e6149b54bbbd504b
Pavel Smirnov <pavel_smirnov1@epam.com>
2021-08-03 20:54:22 +0000

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import 'ketcher-react/dist/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 import { StandaloneStructServiceProvider } from 'ketcher-standalone';
 import { Editor } from 'ketcher-react';
@@ -8,10 +12,18 @@ const structServiceProvider = new StandaloneStructServiceProvider();
 
 function App() {
   return (
-    <Editor
-      staticResourcesUrl={process.env.PUBLIC_URL}
-      structServiceProvider={structServiceProvider}
-    />
+    <Modal centered show size="xl" onHide={() => {}} dialogClassName="ModalDialog" contentClassName="ModalContent">
+      <Modal.Body className="ModalBody">
+        <Editor
+          staticResourcesUrl={process.env.PUBLIC_URL}
+          structServiceProvider={structServiceProvider}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-secondary">Cancel</Button>
+        <Button variant="success">Accept</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
