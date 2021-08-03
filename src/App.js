f674b911a4d29f09e86cce168aeccd85f4ee425b
Pavel Smirnov <pavel_smirnov1@epam.com>
2021-08-03 22:09:55 +0000

    Reproduce without bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import 'ketcher-react/dist/index.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 
 import { StandaloneStructServiceProvider } from 'ketcher-standalone';
 import { Editor } from 'ketcher-react';
@@ -12,18 +8,19 @@ const structServiceProvider = new StandaloneStructServiceProvider();
 
 function App() {
   return (
-    <Modal centered show size="xl" onHide={() => {}} dialogClassName="ModalDialog" contentClassName="ModalContent">
-      <Modal.Body className="ModalBody">
-        <Editor
-          staticResourcesUrl={process.env.PUBLIC_URL}
-          structServiceProvider={structServiceProvider}
-        />
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-secondary">Cancel</Button>
-        <Button variant="success">Accept</Button>
-      </Modal.Footer>
-    </Modal>
+    <div className="Modal">
+      <div className="Dialog DialogCentered">
+        <div className="Content">
+          <div className="Body">
+            <Editor
+              staticResourcesUrl={process.env.PUBLIC_URL}
+              structServiceProvider={structServiceProvider}
+            />
+          </div>
+          <div className="Footer"></div>
+        </div>
+      </div>
+    </div>
   );
 }
 
